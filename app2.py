a1c31963b98012611c04f9d5aa8abc6c2137fed9
SreyviUANN <sreyviuann2000@gmail.com>
2024-02-07 16:15:18 +0700

    main2

---
+++ app2.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+import streamlit as st
+ad = pd.read_csv(
+    'C:/Users/ASUS/Desktop/coding/programing/new project/Advertising Budget and Sales.csv',
+    index_col=[0],
+)
+columns = ["tv", "radio", "news", "sales"]
+ad.columns=columns
+df = ad.copy()
+target = "sales"
+features = df.columns.to_list()
+features.remove(target)
+dict_scaler = dict()
+for col in columns:
+    dict_scaler[col] = StandardScaler().fit(ad[[col]])
+for col in columns:
+    df[col] = dict_scaler[col].transform(X=ad[[col]])
+target = "sales"
+features = df.columns.to_list()
+features.remove(target)
+X = df[features].values
+y = df[target].values
+robot = LinearRegression().fit(X=X, y=y)
+st.title(body="Predict Sales by Advertisement Types")
+#st.number_input(label='tv')
+user_input = dict()
+for col in features:
+    user_input[col]=st.number_input(label=col, value=ad[col].mean())
+#st.write(user_input)
+df_input = pd.DataFrame(data=[user_input])
+df_scaled=df_input.copy()
+for col in features:
+    df_scaled[col] = dict_scaler[col].transform(X=df_input[[col]])
+X_test = df_scaled.values
+st.dataframe(data=df_input)
+st.write(robot.predict(X=df_input.values))
+y_pred = robot.predict(X=X_test)
+y_pred = dict_scaler[target].inverse_transform(y_pred.reshape(-1,1))
+st.write(y_pred)
